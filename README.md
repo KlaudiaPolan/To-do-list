61f6ba761e9ae78ab2f547e9550f4c4e64b7fbb7
KlaudiaPolan <polan.szlachta@gmail.com>
2017-07-10 23:24:44 +0200

    added alert and comments, changed readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-"# To-do-list" 
+To do list created with help of HTML5, CSS3 and JavaScript.
+I made it during Coder's Lab bootcamp.
+
+It's simple application with the following features:
+- Your task should has minimum 6 letters but less than 100 letters
+- Counter tells you how many tasks are left to be completed
+- The "Delete" button deletes task whether it's completed or not
+- The "Complete" button changes the color of task to pink
+- "Remove finished tasks" removes the tasks which are marked as completed
+
+<img src="images/laptop.png">
--- js/app.js
+++ js/app.js
@@ -1,52 +1,58 @@
 document.addEventListener("DOMContentLoaded", function() {
   var submitButton = document.querySelector("#addTaskButton");
   var input = document.querySelector("input");
-
   var withCounter=document.createElement('div');
   var counter=0;
-  var body = document.querySelector(".container");
   withCounter.innerHTML="My tasks: "+counter;
+  var body = document.querySelector(".container");
   body.insertBefore(withCounter, input);
 
-
-  submitButton.addEventListener("click",function(){
-    var text = input.value;
-
-    var taskList = document.querySelector("#taskList");
-    if (text.length > 5 && text.length < 100) {
-      counter++;
-      withCounter.innerHTML="Task to do: "+counter;
-      var newTask=document.createElement('li');
-      var tempLi=taskList.appendChild(newTask);
-      var newH1 = document.createElement("h1");
-      newH1.innerHTML = text;
-      input.value = " ";
-      var textH1=tempLi.appendChild(newH1);
-      var btnDelete = document.createElement("button");
-      btnDelete.innerHTML = "Delete";
-      var buttonDelete=tempLi.appendChild(btnDelete);
-      var btnComplete = document.createElement("button");
-      btnComplete.innerHTML = "Complete";
-      var buttonComplete=tempLi.appendChild(btnComplete);
-
-      buttonComplete.addEventListener("click", function() {
-          textH1.classList.toggle("complete");
-      });
-      buttonDelete.addEventListener("click", function() {
-        counter--;
+  submitButton.addEventListener('click', function (e) {
+      text = input.value;
+      var taskList = document.querySelector("#taskList");
+      if (text.length > 5 && text.length < 100) {
+        //add new task
+        counter++;
         withCounter.innerHTML="Task to do: "+counter;
-        newTask.parentNode.removeChild(newTask);
-      });
+        var newTask=document.createElement('li');
+        var tempLi=taskList.appendChild(newTask);
+        var newH1 = document.createElement("h1");
+        newH1.innerHTML = text;
+        input.value = " ";
+        var textH1=tempLi.appendChild(newH1);
+        var btnDelete = document.createElement("button");
+        btnDelete.innerHTML = "Delete";
+        var buttonDelete=tempLi.appendChild(btnDelete);
+        var btnComplete = document.createElement("button");
+        btnComplete.innerHTML = "Complete";
+        var buttonComplete=tempLi.appendChild(btnComplete);
+        var buttonComplete=tempLi.appendChild(btnComplete);
+        buttonComplete.addEventListener("click", function() {
+            textH1.classList.toggle("complete");
+        });
 
-      var btnRemoveFinishedTasks = document.querySelector("#removeFinishedTasksButton");
+        // delete one task
+        buttonDelete.addEventListener("click", function() {
+          counter--;
+          withCounter.innerHTML="Task to do: "+counter;
+          newTask.parentNode.removeChild(newTask);
+        });
+        // Remove finished tasks
+        var btnRemoveFinishedTasks = document.querySelector("#removeFinishedTasksButton");
+            btnRemoveFinishedTasks.addEventListener("click", function() {
+              for(var i=0;i<counter;i++){
+                if(textH1.classList == "complete"){
+                 newTask.parentNode.removeChild(newTask);
+                 counter--;
+                 withCounter.innerHTML="Task to do: "+counter;
+               }
+             }
+           });
 
-      btnRemoveFinishedTasks.addEventListener("click", function() {
-        counter=0;
-        withCounter.innerHTML="Task to do: "+counter;
-        newTask.parentNode.removeChild(newTask);
-      });
-    }
-  });
+      } else{
+        alert('Your task is too short. It should has min 6 letters but less than 100.');
+      }
+    });
 
     submitButton.addEventListener('mouseover',function(){
       this.style.opacity = '0.8';
